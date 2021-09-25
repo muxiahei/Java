5c641d552dddb6577b8b9578a51a281b7e1a789b
jichenzheng <jchenzheng@elementspeed.com>
2021-09-25 23:48:18 +0800

    动态数组

--- src/DataStructures/DynamicArray/DynamicArray.java
+++ src/DataStructures/DynamicArray/DynamicArray.java
@@ -1,5 +1,6 @@
 package DataStructures.DynamicArray;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Spliterator;
@@ -8,9 +9,9 @@ import java.util.function.Consumer;
 public class DynamicArray<E> implements Iterable<E>{
     private static final int DEFAULT_CAPACITY = 16;
 
-    private int capcity;
-    private int size;
-    private Object[] elements;
+    private int capcity;        //存储容量的大小
+    private int size;           //已经存储元素的大小
+    private Object[] elements;  //存放的数据
 
 
     public DynamicArray(final int capcity) {
@@ -20,10 +21,24 @@ public class DynamicArray<E> implements Iterable<E>{
     }
 
     public DynamicArray() {
-        this.capcity = DEFAULT_CAPACITY;
+        this(DEFAULT_CAPACITY);
     }
 
+    /**
+     * 获取数组下标的元素
+     * @param index
+     * @return
+     */
+    public E get(final int index) {
+        return this.getElement(index);
+    }
+
+    /**
+     * 向数组末尾添加元素
+     * @param element
+     */
     public void add(final E element) {
+        //如果当前数组元素存储已满，则扩容为原来的数组的容量的两倍
         if(this.size == this.elements.length) {
             this.elements = Arrays.copyOf(this.elements, newCapactity(2 * this.size));
         }
@@ -31,13 +46,28 @@ public class DynamicArray<E> implements Iterable<E>{
         this.size ++;
     }
 
+    /**
+     * 修改数组中下标为index的元素
+     * @param index
+     * @param element
+     */
     public void put(final int index, E element) {
         this.elements[index] = element;
     }
 
+    /**
+     * 删除数组中的元素
+     * @param index
+     * @return
+     */
     public E remove(final int index) {
         final E oldElement = getElement(index);
-        return null;
+        fastRemove(this.elements,index);
+
+        if(this.capcity > DEFAULT_CAPACITY && size * 4 <= this.capcity) {
+            this.elements = Arrays.copyOf(elements,newCapactity(this.capcity / 2));
+        }
+        return oldElement;
     }
 
     private void fastRemove(final Object[] elements, final int index) {
@@ -48,9 +78,7 @@ public class DynamicArray<E> implements Iterable<E>{
         elements[this.size = newSize] = null;
     }
 
-    public E get(final int index) {
-        return this.getElement(index);
-    }
+
 
     private int newCapactity(int capcity) {
         this.capcity = capcity;
